f7e7c4703aa9f800ce13542e3fce7dedfae91ad0
Yuto Watanabe <yuto.w51942@gmail.com>
2021-08-04 00:22:55 +0900

    added network-util package and setup cors

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
-module github.com/yuto51942/slide-manager
+module github.com/hello-slide/slide-manager
 
 go 1.16
+
+require (
+	github.com/dapr/go-sdk v1.2.0 // indirect
+	github.com/hello-slide/network-util v0.0.0-20210803145747-04fa8d8e821e // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	networkUtils "github.com/hello-slide/network-util"
+)
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
+}
 
 func main() {
-	fmt.Println("Hello")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/slide/", rootHandler)
+
+	handler := networkUtils.CorsConfig.Handler(mux)
+
+	if err := http.ListenAndServe(":3000", handler); err != nil {
+		panic(err)
+	}
 }
