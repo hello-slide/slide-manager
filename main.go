50b6f694e8e9808553c9e15d106fa3462a94f652
Yuto Watanabe <yuto.w51942@gmail.com>
2021-08-05 05:39:42 +0900

    added handlers

--- main.go
+++ main.go
@@ -10,9 +10,29 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello World"))
 }
 
+func createHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func listHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/slide/", rootHandler)
+	mux.HandleFunc("/slide/create", createHandler)
+	mux.HandleFunc("/slide/list", listHandler)
+	mux.HandleFunc("/slide/edit", editHandler)
+	mux.HandleFunc("/slide/delete", deleteHandler)
 
 	handler := networkUtils.CorsConfig.Handler(mux)
 
